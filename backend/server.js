0de25b32d2429e322a008ed492408dd0084d71a6
piyushchhakuriya <chhakuriyaji@gmail.com>
2026-02-24 15:58:33 +0530

    slash

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 // ✅ CORS should come first
 app.use(cors({
-  origin: "https://calm-taiyaki-97019f.netlify.app/",
+  origin: "https://calm-taiyaki-97019f.netlify.app",
   credentials: true,
 }));
 
